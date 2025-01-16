03ea9d11af5f914fbba20b640c97baae4d4b4f8d
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-01-15 18:03:46 -0600

    WORKING authz authn

--- index.js
+++ index.js
@@ -1,6 +1,133 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+// middleware
+app.use(express.json());
+
+// 8. authentication middleware
+const authMiddleware = async (req, res, next) => {
+    try {
+        // create auth header
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        // Expecting a formate "Bearer <token>"
+        const token = authHeader.split(' ')[1];
+        if (!token) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        // verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decoded) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// 1. Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI
+).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+// 2. Define a schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+// 4. Create a model
+const User = mongoose.model('User', userSchema);
+
+// 6. Create a route to register a new user
+app.post("/api/register", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // check if the user already exists
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).send('User already exists');
+        }
+
+        // create the new user
+        const newUser = new User({
+            username,
+            password: await bcrypt.hash(password, 10)
+        })
+
+        // save the user to the database
+        await newUser.save();
+
+        // return successful message
+        res.status(201).send('User created successfully');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('error registering user');
+    }
+})
+
+// 7. Create a route to login a user
+app.post("/api/login", async (req, res) => {
+    try {
+        // destruct the username and password from the request body
+        const { username, password } = req.body;
+
+        // find the user in the database
+        const user = await User.findOne({ username });
+        // if the user doesn't exist, return an error
+        if (!user) {
+            return res.status(401).send('Invalid username or password');
+        }
+
+        // if the user exists, compare the password
+        const isMatch = await bcrypt.compare(password, user.password);
+        // if the password doesn't match, return an error
+        if (!isMatch) {
+            return res.status(401).send('Invalid username or password');
+        }
+
+        // if the password matches, generate a json web token
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1d'
+        });
+
+        // return the token
+        res.status(200).send(token);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('error logging in');
+    }
+});
+
+// 9. protected route
+app.get('/api/protected', authMiddleware, (req, res) => {
+    res.status(200).send('YOU ARE AUTHORIZED');
+});
 
 app.get('/api/health', (req, res) => {
     res.status(200).send('OK');
